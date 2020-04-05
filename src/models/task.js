1d348d5237ac23475d9d3d796c4e2db9862c2c38
Jason McDermott <jmcdermott@rightpoint.com>
2020-04-05 07:24:59 -0400

    code cleanup

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,6 @@
 const { ObjectID } = require('mongodb')
 const mongoose = require('mongoose')
 
-// create schema
 const taskSchema = new mongoose.Schema({
     description: {
         type: String,
@@ -17,29 +16,5 @@ const taskSchema = new mongoose.Schema({
 })
 
 const Task = mongoose.model('Task', taskSchema)
-
-// Task.updateMany({}, {
-//     $set: {
-//         completed: false
-//     }
-// })
-//     .then(result => console.log(result))
-//     .catch(err => console.log(err))
-
-// const task = new Task({
-//     description: "Go shopping"
-// })
-
-// task.save()
-//     .then(result => console.log(result))
-//     .catch(error => console.log(error))
-
-Task.find({})
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
-
-Task.find({ owner: new ObjectID("5e80e086e7179a17e21dcc45")})
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
 
 module.exports = Task    
--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,6 @@
-const { ObjectID } = require('mongodb')
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-// create schema
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -42,43 +40,5 @@ const userSchema = new mongoose.Schema({
 })
 
 const User = mongoose.model('User', userSchema)
-
-// let jason = new User({
-//     name: "Jason",
-//     email: "JAY@kA.cOm",
-//     password: 'test12345'
-// })
-
-// jason.save()
-// .then(result => console.log(result))
-// .catch(error => console.log(error))
-
-// User.deleteMany({name: 'Jason'})
-// .then(result => console.log(result))
-// .catch(error => console.log(error))
-
-User.find({})
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
-
-User.find({ age: 27 })
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
-
-User.countDocuments({ age: 27 })
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
-
-
-// User.updateOne({_id: new ObjectID("5e80e061e7179a17e21dcc31")}, {
-//     $set: {
-//         name: "Mike"
-//     }
-// }).then(result => console.log(result))
-//     .catch(err => console.log(err))
-
-User.findOne({ _id: new ObjectID("5e80e061e7179a17e21dcc31")})
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
 
 module.exports = User
